570bf0db261b607296323a20c0f0bd852a92bba5
Vineet Joshi <vineet0713@gmail.com>
2017-12-28 19:16:33 -0800

    Make code review changes (fix shortName function and make UI compatibility with iPhone SE).

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -16,18 +16,24 @@ let bananaFanaTemplate = [
 
 // Because shortName and lyrics are pure functions, they should be placed outside of class declaration:
 func shortName(from name: String) -> String {
-    var name = name
-    name = name.lowercased()
+    let name = name.lowercased()
+    var modifiedName = name
     let vowelSet = CharacterSet(charactersIn: "aeioöu")
     var index = 0
-    for character in name.unicodeScalars {
+    
+    for character in modifiedName.unicodeScalars {
         if vowelSet.contains(character) {
             break
         }
         index += 1
     }
-    let start = name.index(name.startIndex, offsetBy: index)
-    return String(name[start..<(name.endIndex)])
+    
+    if index < name.count {
+        let start = modifiedName.index(name.startIndex, offsetBy: index)
+        return String(modifiedName[start..<(name.endIndex)])
+    } else {
+        return name
+    }
 }
 
 func lyrics(for name: String, template: String) -> String {
@@ -56,10 +62,19 @@ class ViewController: UIViewController {
     
     // action connection for "Editing Did End"
     @IBAction func displayLyrics(_ sender: Any) {
+        /*
         if nameField.text!.isEmpty {
             return
         }
-        let capitalizedName = nameField.text!.capitalizingFirstLetter()
+        */
+        
+        // instead of checking if text is empty, I could use a guard statement!
+        guard let name = nameField.text, name.count > 0 else {
+            return
+        }
+        
+        // also trims the trailing whitespaces:
+        let capitalizedName = name.capitalizingFirstLetter().trimmingCharacters(in: .whitespaces)
         nameField.text = capitalizedName
         lyricsView.text = lyrics(for: capitalizedName, template: bananaFanaTemplate)
     }
@@ -73,12 +88,13 @@ class ViewController: UIViewController {
         // tells the TextField to use our view controller as its delegate
         nameField.delegate = self
     }
-
+    
+    // This method has no custom code, so it will not be overriden
+    /*
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    */
 }
 
